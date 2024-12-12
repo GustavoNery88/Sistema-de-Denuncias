960fd3a422bfc8a28eab1806484287bed8b0acff
GustavoNery88 <gusta123sam@gmail.com>
2024-12-12 15:19:47 -0300

    save

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,32 @@
 // Configuração do spinner
 const opts = {
-  lines: 12,            // Número de linhas
-  length: 7,            // Tamanho do "giro"
-  width: 4,             // Largura das linhas
-  radius: 10,           // Raio do spinner
-  scale: 1.5,           // Escala
-  color: '#000',        // Cor do spinner
+  lines: 12,           
+  length: 7,            
+  width: 4,            
+  radius: 10,           
+  scale: 1.5,      
+  color: '#000',        
   fadeColor: 'transparent',
-  speed: 1,             // Velocidade
-  trail: 60,            // Traço de cor
-  shadow: false,        // Sombra
-  hwaccel: false,       // Aceleração de hardware
-  position: 'absolute'  // Posição do spinner
+  speed: 1,            
+  trail: 60,      
+  shadow: false,       
+  hwaccel: false,     
+  position: 'absolute' 
 };
 
 // Função para exibir o spinner
 function showSpinner() {
   const spinnerElement = document.getElementById('spinner');
-  spinnerElement.style.display = 'block'; // Mostrar o spinner
-  new Spinner(opts).spin(spinnerElement); // Iniciar o spinner
+  spinnerElement.style.display = 'block';
+  new Spinner(opts).spin(spinnerElement); 
 }
 
 // Função para ocultar o spinner
 function hideSpinner() {
   const spinnerElement = document.getElementById('spinner');
-  spinnerElement.style.display = 'none'; // Ocultar o spinner
+  spinnerElement.style.display = 'none'; 
 }
 
-// Adiciona um evento 'beforeunload' para garantir que o spinner apareça durante a navegação
 window.addEventListener('beforeunload', function () {
   showSpinner();
 });
--- routes/agentes.js
+++ routes/agentes.js
@@ -19,7 +19,6 @@ router.post('/cadastrar', ensureAuthenticatedJWT, ensureAdmin, async (req, res) 
     const cpfFormatado = cpf.replace(/\D/g, '');
 
     try {
-        // Checa se o CPF já está cadastrado
         const agenteJaExistente = await Agente.findOne({ cpf: cpfFormatado });
         const emailJaExistente = await Agente.findOne({ email: email });
 
@@ -45,10 +44,10 @@ router.post('/cadastrar', ensureAuthenticatedJWT, ensureAdmin, async (req, res) 
             cpf: cpfFormatado,
             email,
             senha,
-            admin: admin === 'on' // Checkbox do campo admin
+            admin: admin === 'on'
         });
 
-        await novoAgente.save(); // Salva o agente no banco de dados
+        await novoAgente.save(); 
 
         req.flash('success', 'Agente cadastrado com sucesso!');
         res.redirect('/agente/usuarios');
@@ -63,12 +62,12 @@ router.post('/cadastrar', ensureAuthenticatedJWT, ensureAdmin, async (req, res) 
 // Rota para exibir todas as denúncias novas (Recebidas)
 router.get('/novasDenuncias', ensureAuthenticatedJWT, async (req, res) => {
     try {
-        const denunciasNovas = await Denuncia.find({ status: 'Recebida' }); // Filtra denúncias pelo status
-        res.render('agente/novasDenuncias', { denuncias: denunciasNovas, user: req.user }); // Passa o usuário para a view
+        const denunciasNovas = await Denuncia.find({ status: 'Recebida' }); 
+        res.render('agente/novasDenuncias', { denuncias: denunciasNovas, user: req.user });
     } catch (error) {
         console.error(error);
         req.flash('error', 'Erro ao buscar denúncias.');
-        res.redirect('/'); // Redireciona em caso de erro
+        res.redirect('/'); 
     }
 });
 
@@ -90,7 +89,6 @@ router.get('/visualizar/:id', ensureAuthenticatedJWT, async (req, res) => {
     const denunciaId = req.params.id;
 
     try {
-        // Busca a denúncia pelo ID para visualização
         const denuncia = await Denuncia.findById(denunciaId);
         if (!denuncia) {
             req.flash('error', 'Denúncia não encontrada.');
@@ -110,7 +108,6 @@ router.get('/editarDenuncia/:id', ensureAuthenticatedJWT, async (req, res) => {
     const denunciaId = req.params.id;
 
     try {
-        // Busca a denúncia pelo ID para visualização
         const denuncia = await Denuncia.findById(denunciaId);
         if (!denuncia) {
             req.flash('error', 'Denúncia não encontrada.');
@@ -130,17 +127,16 @@ router.get('/editarDenuncia/:id', ensureAuthenticatedJWT, async (req, res) => {
 // Rota para se atribuir da denúncia
 router.post('/atribuir/:id', ensureAuthenticatedJWT, async (req, res) => {
     const denunciaId = req.params.id;
-    const agenteId = req.user.id; // ID do agente logado
+    const agenteId = req.user.id;
 
     try {
-        // Atualiza a denúncia com o ID do agente e muda o status para 'Em análise'
         await Denuncia.findByIdAndUpdate(denunciaId, {
             agenteAtribuido: agenteId,
             status: 'Em análise'
         });
 
         req.flash('success', 'Você se atribuiu a esta denúncia com sucesso.');
-        res.redirect('/agente/novasDenuncias'); // Redireciona para a página de novas denúncias
+        res.redirect('/agente/novasDenuncias'); 
     } catch (error) {
         console.error(error);
         req.flash('error', 'Erro ao atribuir-se à denúncia.');
@@ -153,7 +149,6 @@ router.post('/editarDenuncia', ensureAuthenticatedJWT, async (req, res) => {
     const { id, status } = req.body;
 
     try {
-        // Atualiza a denúncia com o novo status
         await Denuncia.findByIdAndUpdate(id, { status });
 
         req.flash('success', 'Status da denúncia atualizado com sucesso!');
@@ -171,7 +166,6 @@ router.get('/denunciasAtribuidas', ensureAuthenticatedJWT, async (req, res) => {
     try {
         const denunciasAtribuidas = await Denuncia.find({ agenteAtribuido: req.user.id });
 
-        // Busca o agente pelo ID
         const agente = await Agente.findById(req.user.id);
 
         res.render('agente/denunciasAtribuidas', {
@@ -193,7 +187,7 @@ router.post('/desatribuir/:id', ensureAuthenticatedJWT, async (req, res) => {
     try {
         await Denuncia.findByIdAndUpdate(denunciaId, {
             agenteAtribuido: null,
-            status: 'Recebida' // Retorna o status para "Recebida"
+            status: 'Recebida' 
         });
 
         req.flash('success', 'Você se desatribuiu desta denúncia com sucesso.');
@@ -208,8 +202,8 @@ router.post('/desatribuir/:id', ensureAuthenticatedJWT, async (req, res) => {
 // Rota para exibir os usuários cadastrados
 router.get('/usuarios', ensureAuthenticatedJWT, ensureAdmin, async (req, res) => {
     try {
-        const usuarios = await Agente.find({}, 'nome cpf email'); // Inclua email caso precise exibir essa informação
-        res.render('agente/usuarios', { usuarios, user: req.user }); // Renderiza a view e passa os agentes como dados
+        const usuarios = await Agente.find({}, 'nome cpf email'); 
+        res.render('agente/usuarios', { usuarios, user: req.user });
     } catch (error) {
         console.error(error);
         req.flash("error", "Erro ao buscar os usuários.");
@@ -223,7 +217,6 @@ router.get('/usuarioVisualizar/:id', ensureAuthenticatedJWT, ensureAdmin, async 
     const usuarioId = req.params.id;
 
     try {
-        // Busca o agente pelo ID e exclui o campo 'senha' dos dados retornados
         const usuario = await Agente.findById(usuarioId).select('nome cpf email');
         if (!usuario) {
             req.flash('error', 'Usuário não encontrado.');
@@ -262,13 +255,11 @@ router.get('/usuarioEditar/:id', ensureAuthenticatedJWT, ensureAdmin, async (req
 router.post('/usuarioEditar/:id', ensureAuthenticatedJWT, ensureAdmin, async (req, res) => {
     const usuarioId = req.params.id;
     const { nome, cpf, email } = req.body;
-    const cpfFormatado = cpf.replace(/\D/g, ''); // Formatar CPF
+    const cpfFormatado = cpf.replace(/\D/g, ''); 
 
     try {
-        // Objeto de atualização
         const updates = { nome, cpf: cpfFormatado, email };
 
-        // Verificação de duplicidade de CPF e e-mail, excluindo o próprio usuário
         const agenteJaExistente = await Agente.findOne({ cpf: cpfFormatado, _id: { $ne: usuarioId } });
         const emailJaExistente = await Agente.findOne({ email: email, _id: { $ne: usuarioId } });
 
@@ -285,7 +276,6 @@ router.post('/usuarioEditar/:id', ensureAuthenticatedJWT, ensureAdmin, async (re
             return res.redirect(`/agente/usuarioEditar/${usuarioId}`);
         }
 
-        // Atualiza o usuário no banco de dados
         await Agente.findByIdAndUpdate(usuarioId, updates);
 
         req.flash('success', 'Usuário atualizado com sucesso!');
@@ -302,8 +292,6 @@ router.get('/usuarioExcluir/:id', ensureAuthenticatedJWT, ensureAdmin, async (re
     const usuarioId = req.params.id;
 
     try {
-        // agenteAtribuido é um compo na tabela denuncias
-        // Verifica se há denúncias atribuídas ao usuário
         const denunciasAtribuidas = await Denuncia.find({ agenteAtribuido: usuarioId });
 
         if (denunciasAtribuidas.length > 0) {
@@ -311,7 +299,6 @@ router.get('/usuarioExcluir/:id', ensureAuthenticatedJWT, ensureAdmin, async (re
             return res.redirect('/agente/usuarios');
         }
 
-        // Exclui o usuário
         await Agente.findByIdAndDelete(usuarioId);
         req.flash('success', 'Usuário excluído com sucesso!');
         res.redirect('/agente/usuarios');
@@ -333,21 +320,18 @@ router.post('/login', async (req, res) => {
     const { cpf, senha } = req.body;
 
     try {
-        // Busca o agente pelo CPF
         const agente = await Agente.findOne({ cpf, });
         if (!agente) {
             req.flash('error', 'CPF não encontrado!');
             return res.redirect('/agente/login');
         }
 
-        // Verifica se a senha está correta
         const isMatch = await bcrypt.compare(senha, agente.senha);
         if (!isMatch) {
             req.flash('error', 'Senha incorreta');
             return res.redirect('/agente/login');
         }
 
-        // Gera o JWT
         const payload = {
             id: agente._id,
             nome: agente.nome,
@@ -356,7 +340,6 @@ router.post('/login', async (req, res) => {
 
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
 
-        // Define o token como um cookie HTTP-only
         res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
 
         req.flash('success', 'Usuário logado com sucesso!');
@@ -370,7 +353,6 @@ router.post('/login', async (req, res) => {
 
 // Rota para Logout
 router.get('/logout', (req, res) => {
-    // Remove o cookie do token
     res.clearCookie('token');
     req.flash('success', 'Saiu!');
     res.redirect('/agente/login');
@@ -388,10 +370,10 @@ router.get('/codigoVerificacao', (req, res) => {
 
 // Configuração do Nodemailer para envio de e-mails
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // Serviço de e-mail, pode usar Gmail, Outlook, etc.
+    service: 'gmail', 
     auth: {
-        user: process.env.EMAIL_USER, // Seu e-mail
-        pass: process.env.EMAIL_PASS  // Sua senha 
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS 
     }
 });
 
@@ -401,7 +383,6 @@ router.post('/gerar-codigo-verificacao', async (req, res) => {
     const cpfFormatado = cpf.replace(/\D/g, '');
 
     try {
-        // Verifica se o CPF e e-mail é válido para o agente
         const agente = await Agente.findOne({ email, cpf: cpfFormatado });
         if (!agente) {
             req.flash('error', 'E-mail ou CPF inválido para o agente!');
@@ -409,19 +390,16 @@ router.post('/gerar-codigo-verificacao', async (req, res) => {
         }
 
 
-        // Gerar token de verificação
         const token = crypto.randomBytes(3).toString('hex');
 
-        // Atualizar somente os campos necessários
         await Agente.updateOne(
             { _id: agente._id },
             {
                 resetToken: token,
-                resetTokenExpiration: Date.now() + 3600000, // Expira em 1 hora
+                resetTokenExpiration: Date.now() + 3600000,
             }
         );
 
-        // Enviar e-mail com o token
         const mailOptions = {
             from: process.env.EMAIL_USER,
             to: email,
@@ -451,20 +429,17 @@ router.post('/verificar-codigo', async (req, res) => {
     const cpfFormatado = cpf.replace(/\D/g, '');
 
     try {
-        // Gera o hash da nova senha
+        
         const salt = await bcrypt.genSalt(10);
         const novaSenhaCriptografada = await bcrypt.hash(novaSenha, salt);
 
-        // Verifica se o token é válido para o agente com o e-mail fornecido
         const agente = await Agente.findOne({ cpf: cpfFormatado, resetToken: token });
 
-        // Se não encontrar o agente ou o token não corresponder, retorna erro
         if (!agente) {
             req.flash('error', 'Token inválido para o agente.');
             return res.redirect('/agente/codigoVerificacao');
         }
 
-        // Verifica se o token expirou
         else if (agente.resetTokenExpiration < Date.now()) {
             req.flash('error', 'Token expirado.');
             return res.redirect('/agente/codigoVerificacao');
@@ -477,15 +452,14 @@ router.post('/verificar-codigo', async (req, res) => {
 
         
         await Agente.updateOne(
-            { _id: agente._id }, // Encontra o agente pelo _id (garante que estamos atualizando o agente correto)
+            { _id: agente._id },
             {
-                senha: novaSenhaCriptografada, // Atualiza a senha do agente
-                resetToken: null,             // Remove o token
-                resetTokenExpiration: null    // Remove a validade do token
+                senha: novaSenhaCriptografada,
+                resetToken: null,             
+                resetTokenExpiration: null   
             }
         );
 
-        // Sucesso na redefinição de senha
         req.flash('success', 'Senha redefinida com sucesso!');
         res.redirect('/agente/login');
     } catch (error) {
